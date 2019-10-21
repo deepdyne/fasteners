ea6b932cb9ca7c2b4390bf5a721df8f7c246c272
mgoldchild <mgoldchild@gmail.com>
2019-10-21 15:59:52 +0900

    add script

---
+++ voc_summary.py
@@ -0,0 +1,30 @@
+import glob
+import os
+
+sets = ["train", "val"]
+classes = ["fastener"]
+
+DIR_NAME = "fastener/output/YOLO-PascalVOC-export/ImageSets/Main/"
+
+os.chdir(DIR_NAME)
+
+for file_type in ["train", "val"]:
+    file_names = []
+    for file_name in glob.glob("*_{}.txt".format(file_type)):
+        file_names.append(file_name)
+
+    image_ids = []
+    print("file_names", file_names)
+    for file_name in file_names:
+        image_file_names = (
+            open(file_name)
+            .read()
+            .strip()
+            .split()[0::2]  # [0::2] means only takes odd no items
+        )
+        image_file_ids = list(map(lambda x: os.path.splitext(x)[0], image_file_names))
+        image_ids = image_file_ids
+
+    with open("{}.txt".format(file_type), "w") as f:
+        for image_id in image_ids:
+            f.write("%s\n" % image_id)
